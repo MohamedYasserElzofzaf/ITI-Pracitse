39472035238ed427333f5fcc50056987f3ed2e0a
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-20 00:25:38 +0200

    Working with ES6

--- ES6/main.js
+++ ES6/main.js
@@ -18,6 +18,27 @@ function sayHi() {
 }
 sayHi();
 
+// ====================Object============================================
+const person = {
+    name: "Yasser",
+    job: function() {},
+    age() {},
+};
+
+person.job();
+person["name"] = "Mohammed";
+
+// =====================this============================================
+const man = {
+    name: "Yasser",
+    walk() {
+        console.log(this);
+    },
+};
+man.walk();
+const walk = man.walk;
+walk();
+
 // =====================Arrow function ==================================
 var fun = (num) => {
     return num + num;
@@ -25,3 +46,24 @@ var fun = (num) => {
 var fun1 = (num) => num + num;
 console.log(fun(5));
 console.log(fun1(5));
+
+// ============================spread operator ==============================
+const first = [1, 2, 3, 4, 5];
+const second = [6, 7, 8, 9];
+const combined = first.concat(second);
+const combined1 = {...first, ...second };
+console.log(combined);
+console.log(combined1);
+
+// ============================destructuring object==========================
+const address = {
+    street: "",
+    city: "",
+    country: "",
+};
+
+const street = address.street;
+const city = address.city;
+const country = address.country;
+// we can use instead
+const { street, city, country } = address;
