fecb70e51a721723e7cf766b11e8734bfec3ec0a
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-31 22:25:33 +0200

    working on Angular practis

--- Angular/Days/src/app/app.module.ts
+++ Angular/Days/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ComponentInteractionComponent } from './component-interaction/component-interaction.component';
 import { TestValidComponent } from './components/test-valid/test-valid.component';
+import { LoginComponent } from './components/component-interaction/login/login.component';
+import { HomeComponent } from './components/component-interaction/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentInteractionComponent,
-    TestValidComponent
+    TestValidComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
