44e2529e220f27c0b7c0ba2a755417545055d33d
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-05-31 16:21:26 +0200

    working on Angular practis

---
+++ Angular/Days/src/app/components/test-valid/test-valid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-valid',
+  templateUrl: './test-valid.component.html',
+  styleUrls: ['./test-valid.component.css']
+})
+export class TestValidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
