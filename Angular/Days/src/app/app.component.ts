2f40f37614b77f7ce473f6e7d6f365f0edad6b24
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-01 16:12:35 +0200

    working on Angular practis

--- Angular/Days/src/app/app.component.ts
+++ Angular/Days/src/app/app.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Days';
-  parentData: string="Hello from the parent"
+  parentData: string = 'Hello from the parent';
+  parentDataFromLogin = '';
+  GetData(data: any) {
+    this.parentDataFromLogin = data;
+  }
 }
--- Angular/Days/src/app/components/component-interaction/login/login.component.ts
+++ Angular/Days/src/app/components/component-interaction/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // setTimeout(() => {
+    //   this.ourEvent.emit(this.loginData);
+    // }, 2000);
+  }
+  loginData = 'Data from login';
+  ngOnInit(): void {}
+  FireData() {
+    this.ourEvent.emit(this.loginData);
   }
 
+  @Output() ourEvent = new EventEmitter();
 }
