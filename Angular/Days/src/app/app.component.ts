799e2eb84e0d32f9c6309321b5f20da9ccab05cb
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-01 14:38:42 +0200

    working on Angular practis

--- Angular/Days/src/app/app.component.ts
+++ Angular/Days/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Days';
+  parentData: string="Hello from the parent"
 }
--- Angular/Days/src/app/app.module.ts
+++ Angular/Days/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ComponentInteractionComponent } from './component-interaction/component-interaction.component';
 import { TestValidComponent } from './components/test-valid/test-valid.component';
 import { LoginComponent } from './components/component-interaction/login/login.component';
 import { HomeComponent } from './components/component-interaction/home/home.component';
@@ -10,7 +9,6 @@ import { HomeComponent } from './components/component-interaction/home/home.comp
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentInteractionComponent,
     TestValidComponent,
     LoginComponent,
     HomeComponent
