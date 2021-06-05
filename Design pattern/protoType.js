017b44efaef17b5e619fef5eb4d989dd622e50c9
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-06 00:24:21 +0200

    Begin with Design Pattern

--- Design pattern/protoType.js
+++ Design pattern/protoType.js
@@ -1,11 +1,16 @@
-var peopleFactory = function(name, age, state) {
-    var temp = {};
-    temp.name = name;
-    temp.age = age;
-    temp.state = state;
+var peopleProto = function() {};
 
-    temp.printPerson = function() {
-        console.log(`${this.name} , ${this.age} , ${this.state}`);
-    };
-    return temp;
+// default values
+peopleProto.prototype.name = "no name";
+peopleProto.prototype.age = 0;
+peopleProto.prototype.state = "no state";
+
+peopleProto.prototype.printType = function() {
+    console.log(`${this.name} , ${this.age} , ${this.state}`);
 };
+const person1 = new peopleProto();
+person1.name = "Ali";
+person1.age = 27;
+person1.state = "cairo";
+
+person1.printType();
