3b9d99a001771f02c6ba5780d152f259032b90fd
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-14 01:32:57 +0200

    working with node js => Debuag

--- Node js/Day1.js
+++ Node js/Day1.js
@@ -1,5 +1,11 @@
+/*
 const calc = (n1, n2) => {
     return Number(n1) + Number(n2);
 };
 
 console.log(calc(process.argv[2], process.argv[3]));
+*/
+// Read from file
+const fs = require("fs");
+let file = fs.readFileSync("./resources/txt.txt");
+console.log(file);
