63b9d2c276857c8f67f30298fee78242be810c80
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-14 22:40:01 +0200

    working with node js =>take input && read& write the operation in  file

--- Node js/calc.js
+++ Node js/calc.js
@@ -13,3 +13,26 @@ if (operation) {
         });
     });
 }
+
+// Handle args
+function handleArgs() {
+    let operation;
+    switch (process.argv[2]) {
+        case "plus":
+        case "+":
+            {
+                operation = {
+                    operator: process.argv[2],
+                    operand1: process.argv[3],
+                    operand2: process.argv[4],
+                    result: Number(process.argv[3]) + Number(process.argv[4]),
+                };
+                break;
+            }
+        default:
+            {
+                console.log("specify valid operator");
+            }
+    }
+    return operation;
+}
