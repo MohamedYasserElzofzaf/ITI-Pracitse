56d39d585d3145f854403f7cb67cd16da1fbfb90
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-14 18:52:53 +0200

    working with node js => write in  file

--- Node js/Day1.js
+++ Node js/Day1.js
@@ -8,4 +8,5 @@ console.log(calc(process.argv[2], process.argv[3]));
 // Read from file
 const fs = require("fs");
 let file = fs.readFileSync("./resources/txt.txt", "utf-8");
+fs.writeFileSync("./resources/txt.txt", file + "added from node\n");
 console.log(file);
