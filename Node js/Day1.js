4ee7aee3002c92f5d4257004fb0367d40ec8df84
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-14 18:29:10 +0200

    working with node js => Read from file

--- Node js/Day1.js
+++ Node js/Day1.js
@@ -7,5 +7,5 @@ console.log(calc(process.argv[2], process.argv[3]));
 */
 // Read from file
 const fs = require("fs");
-let file = fs.readFileSync("./resources/txt.txt");
+let file = fs.readFileSync("./resources/txt.txt", "utf-8");
 console.log(file);
