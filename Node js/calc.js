a5f165574d2b012c1013fa7a5bab3f68352f1e90
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-14 22:28:23 +0200

    working with node js =>take input && read& write the operation in  file

--- Node js/calc.js
+++ Node js/calc.js
@@ -1,1 +1,15 @@
-const fs = require("fs");
+const { readFile, writeFile } = require("fs");
+let operations;
+let operation = handleArgs();
+if (operation) {
+    readFile("./resources/operations.json", "utf-8", (err, data) => {
+        if (err) operations = [];
+        else operations = JSON.parse(data);
+
+        operations.push(operation);
+        data = JSON.stringify(operations);
+        writeFile("./resources/operations.json", data, (err) => {
+            console.log(err);
+        });
+    });
+}
