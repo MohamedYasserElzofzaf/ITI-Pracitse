f38ac5aef82c1bff974b9e4292af7ddb53894e58
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-22 12:51:18 +0200

    React project

--- React/Day1/myapp/src/App.js
+++ React/Day1/myapp/src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
             onClick = {
                 () => {
                     console.log("clicked");
+                    this.setState({ name: "Mohamed" });
                 }
             }
             />{" "} <
