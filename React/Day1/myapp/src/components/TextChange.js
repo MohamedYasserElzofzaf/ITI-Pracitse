8905c68534ffb75141d14464750b25d5895d30c3
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-06-24 20:26:42 +0200

    Adding search bar in react

---
+++ React/Day1/myapp/src/components/TextChange.js
@@ -0,0 +1,12 @@
+const Text = () => {
+    const clickHandler = () => {
+        console.log("clicked");
+    };
+    return ( <
+        p onClick = { clickHandler } >
+        Rebum voluptua voluptua consetetur et lorem et gubergren erat aliquyam consetetur, magna magna nonumy accusam ipsum dolor eirmod, no ipsum. <
+        /p>
+    );
+};
+
+export default Text;
