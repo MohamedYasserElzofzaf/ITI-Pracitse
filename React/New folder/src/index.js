7f1b996b1f6761ec41b0e19678df11f50b6fbc66
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-01 16:26:26 +0200

    adding the route and switch to specifiy the path

--- React/New folder/src/index.js
+++ React/New folder/src/index.js
@@ -1,25 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Home from "./components/home";
+import Profile from "./components/profile";
+import Users from "./components/users";
+import UserDetails from "./components/user-details";
+import Nofound from "./components/notFound";
 
 ReactDOM.render( <
     BrowserRouter > { " " } <
-    Route path = "/"
-    component = { Home }
-    /> <
+    App / > { " " } <
+    Switch >
+    <
+    Route path = "/user/id:"
+    component = { UserDetails }
+    />{" "} <
     Route path = "/profile"
     component = { Profile }
-    /> <
+    />{" "} <
     Route path = "/users"
     component = { Users }
-    /> <
-    Route path = "/user-details"
-    component = { UserDetails }
-    /> <
-    App / >
-    <
+    />{" "} <
+    Route exact path = "/"
+    component = { Home }
+    />{" "} <
+    Route path = "*"
+    component = { Nofound }
+    />{" "} <
+    /Switch>{" "} <
     /BrowserRouter>,
     document.getElementById("root")
 );
