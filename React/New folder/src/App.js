2fe36d58bdb303a9a77fe2eff6cdf114673174ae
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-01 13:01:26 +0200

    adding app

--- React/New folder/src/App.js
+++ React/New folder/src/App.js
@@ -1,32 +1,4 @@
-import { Component } from "react";
-import Search from "./components/search";
-import NewsList from "./components/news-list";
-import NEWSLIST from "./news-list.json";
-import "./App.css";
-
-export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            newsList: NEWSLIST,
-            filteredList: NEWSLIST,
-        };
-    }
-    filterNewsList = (keywords) => {
-        //   filter
-        const filteredList = this.state.newsList.filter((item) =>
-            item.user.includes(keywords)
-        );
-        this.setState({ filteredList: filteredList });
-    };
-    render() {
-        return ( <
-            div > { " " } <
-            Search onKeywordsChange = { this.filterNewsList }
-            />{" "} <
-            NewsList list = { this.state.filteredList }
-            />{" "} <
-            /div>
-        );
-    }
-}
+const App = () => {
+    return <div > App < /div>;
+};
+export default App;
