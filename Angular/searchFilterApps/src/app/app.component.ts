bb82ae05ac792675fecc63d448413a189f29f9c5
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-05 16:49:27 +0200

    Add filter of ng2-search-filter in our component

--- Angular/searchFilterApps/src/app/app.component.ts
+++ Angular/searchFilterApps/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface Country {
+  name: string;
+  flag: string;
+  area: number;
+  population: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'searchFilterApps';
+export class AppComponent implements OnInit {
+  searchTerm: string;
+  countries: Country[];
+  term: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Country[]>('./assets/data/countries.json')
+      .subscribe((data: Country[]) => {
+        this.countries = data;
+      });
+  }
 }
