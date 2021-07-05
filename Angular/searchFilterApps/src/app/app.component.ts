b69d593c5ece3221f6642fe3687703f81f955f07
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-05 17:51:45 +0200

    d search bar input field and our custom searchFilter pipe on ngFor loop of list.

--- Angular/searchFilterApps/src/app/app.component.ts
+++ Angular/searchFilterApps/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+/* import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 interface Country {
@@ -26,5 +26,44 @@ export class AppComponent implements OnInit {
       .subscribe((data: Country[]) => {
         this.countries = data;
       });
+  }
+}
+*/
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface Country {
+  name: string;
+  flag: string;
+  area: number;
+  population: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  searchTerm: string = '';
+  countries!: Country[];
+  allCountries!: Country[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Country[]>('./assets/data/countries.json')
+      .subscribe((data: Country[]) => {
+        this.countries = data;
+        this.allCountries = this.countries;
+      });
+  }
+
+  search(value: string): void {
+    this.countries = this.allCountries.filter((val) =>
+      val.name.toLowerCase().includes(value)
+    );
   }
 }
