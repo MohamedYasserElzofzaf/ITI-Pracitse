0a82e41a6d19d1c5b2ea18422e33272176dbdce1
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-07-05 17:19:31 +0200

    Edit search-filter.pipe.ts file to filter list based on user type text criteria of input field data.

--- Angular/searchFilterApps/src/app/search-filter.pipe.ts
+++ Angular/searchFilterApps/src/app/search-filter.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'searchFilter'
+  name: 'searchFilter',
 })
 export class SearchFilterPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(list: any[], filterText: string): any {
+    return list
+      ? list.filter(
+          (item) => item.name.search(new RegExp(filterText, 'i')) > -1
+        )
+      : [];
   }
-
 }
